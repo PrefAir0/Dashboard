d8255c47ab73ccbbda88bc936d0e8e469fbac6df
Ivan <vanas0576@gmail.com>
2025-12-03 12:30:34 +0500

    перенос старого проекта

---
+++ src/pages/Dashboard_2/dashboard2.tsx
@@ -0,0 +1,11 @@
+
+
+export function Dashboard_2() {
+  return (
+    <div className="">
+
+    </div>
+  );
+}
+
+export default Dashboard_2;
---
+++ src/pages/Dashboard_3/dashboard3.tsx
@@ -0,0 +1,11 @@
+
+
+export function Dashboard_3() {
+  return (
+    <div className="">
+        
+    </div>
+  );
+}
+
+export default Dashboard_3;
